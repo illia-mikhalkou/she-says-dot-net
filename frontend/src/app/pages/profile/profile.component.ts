6bd980703dfe5e73fbd2738f6cd194132eac2c90
Illia Mikhalkou <illia.mikhalkou@gmail.com>
2018-06-16 13:48:46 +0300

    Profile changes, side menu style changes.

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs/internal/Subscription';
+
+import { UserProfile, ProfileAchievement } from './../../interfaces';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription;
+
+  userId: number;
+
+  userProfile: UserProfile;
+  achievements: ProfileAchievement[] = [];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(params => {
+      this.userId = +params['id'];
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
